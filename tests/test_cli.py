33466614586bc3520db8075f5db0d2b07c957135
Floyd Hightower <floyd.hightower27@gmail.com>
2019-02-08 00:43:33 -0400

    Adding test for CLI

---
+++ tests/test_cli.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from click.testing import CliRunner
+
+from ioc_finder import ioc_finder
+
+
+def test_ioc_parsing_cli():
+    runner = CliRunner()
+    result = runner.invoke(ioc_finder.cli_find_iocs, ["This is just an example.com https://example.org/test/bingo.php"])
+    assert result.exit_code == 0
+    print(result.output.strip())
+    assert result.output.strip() == """{
+    "asns": [],
+    "bitcoin_addresses": [],
+    "complete_email_addresses": [],
+    "cves": [],
+    "domains": [
+        "example.org",
+        "example.com"
+    ],
+    "email_addresses": [],
+    "google_adsense_publisher_ids": [],
+    "google_analytics_tracker_ids": [],
+    "ipv4_cidrs": [],
+    "ipv4s": [],
+    "ipv6s": [],
+    "md5s": [],
+    "registry_key_paths": [],
+    "sha1s": [],
+    "sha256s": [],
+    "sha512s": [],
+    "urls": [
+        "https://example.org/test/bingo.php"
+    ]
+}"""
+
+
+def test_cli_without_domain_from_url_parsing():
+    runner = CliRunner()
+    result = runner.invoke(ioc_finder.cli_find_iocs, ["This is just an example.com https://example.org/test/bingo.php", "--no_url_domain_parsing"])
+    assert result.exit_code == 0
+    print(result.output.strip())
+    assert result.output.strip() == """{
+    "asns": [],
+    "bitcoin_addresses": [],
+    "complete_email_addresses": [],
+    "cves": [],
+    "domains": [
+        "example.com"
+    ],
+    "email_addresses": [],
+    "google_adsense_publisher_ids": [],
+    "google_analytics_tracker_ids": [],
+    "ipv4_cidrs": [],
+    "ipv4s": [],
+    "ipv6s": [],
+    "md5s": [],
+    "registry_key_paths": [],
+    "sha1s": [],
+    "sha256s": [],
+    "sha512s": [],
+    "urls": [
+        "https://example.org/test/bingo.php"
+    ]
+}"""
