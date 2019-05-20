ae422333fd67134560f9e5c0d2a9e0e41fb92831
Floyd Hightower <floyd.hightower27@gmail.com>
2019-05-20 09:43:50 -0400

    Adding hypothesis testing
    Fixes #44

--- tests/test_with_hypothesis.py
+++ tests/test_with_hypothesis.py
@@ -1,31 +1,78 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""Test the URL parsing against the urls here: https://mathiasbynens.be/demo/url-regex."""
+"""Using hypothesis (https://hypothesis.readthedocs.io/en/latest/index.html) to test the finder."""
 
-from hypothesis import given
-from hypothesis.provisional import domains, urls
+from hypothesis import given, settings
+from hypothesis.provisional import domains, urls, ip4_addr_strings, ip6_addr_strings
 
 from ioc_finder import find_iocs
 
 
 @given(urls())
+@settings(deadline=None)
 def test_url_parsing(url):
+    url = url.lower()
     iocs = find_iocs(url)
+    failure = False
+
     try:
         assert len(iocs['urls']) == 1
         assert iocs['urls'][0] == url
     except AssertionError as e:
+        failure = True
         print('Failed on url: {}'.format(url))
+
+    if failure:
+        raise AssertionError('Error parsing urls')
 
 
 @given(domains())
+@settings(deadline=None)
 def test_domain_parsing(domain):
+    domain = domain.lower()
     iocs = find_iocs(domain)
+    failure = False
+
     try:
         assert len(iocs['domains']) == 1
         assert iocs['domains'][0] == domain
     except AssertionError as e:
+        failure = True
         print('Failed on domain: {}'.format(domain))
 
+    if failure:
+        raise AssertionError('Error parsing domains')
 
-# TODO: implement similar tests for ipvX strings
+
+@given(ip4_addr_strings())
+@settings(deadline=None)
+def test_ipv4_parsing(ipv4):
+    iocs = find_iocs(ipv4)
+    failure = False
+
+    try:
+        assert len(iocs['ipv4s']) == 1
+        assert iocs['ipv4s'][0] == ipv4
+    except AssertionError as e:
+        failure = True
+        print('Failed on ipv4: {}'.format(ipv4))
+
+    if failure:
+        raise AssertionError('Error parsing ipv4s')
+
+
+@given(ip6_addr_strings())
+@settings(deadline=None)
+def test_ipv6_parsing(ipv6):
+    iocs = find_iocs(ipv6)
+    failure = False
+
+    try:
+        assert len(iocs['ipv6s']) == 1
+        assert iocs['ipv6s'][0] == ipv6
+    except AssertionError as e:
+        failure = True
+        print('Failed on ipv6: {}'.format(ipv6))
+
+    if failure:
+        raise AssertionError('Error parsing ipv6s')
