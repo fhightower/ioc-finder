4eddd7bcaf4fbda466d580bbde61df53100f83db
Floyd Hightower <floyd.hightower27@gmail.com>
2019-07-03 11:48:58 -0400

    Bump version: 1.2.19 → 2.0.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ requirements = [
 
 setup(
     name='ioc_finder',
-    version='1.2.19',
+    version='2.0.0',
     description="Python package for finding and parsing indicators of compromise from text.",
     entry_points={
         'console_scripts': [
