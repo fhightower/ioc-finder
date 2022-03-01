167c1c386ec6a6371040137202e084ea04593ba2
Floyd Hightower <floyd.hightower27@gmail.com>
2022-03-01 06:49:50 -0500

    Adding helpful comment

--- tests/data/__init__.py
+++ tests/data/__init__.py
@@ -2,4 +2,5 @@ from .cves import CVE_DATA
 from .domains import DOMAIN_DATA
 
 TEST_LISTS = [CVE_DATA, DOMAIN_DATA]
+# flatten TEST_LISTS into a single list where each element is a single test case
 ALL_TESTS = [val for sublist in TEST_LISTS for val in sublist]
