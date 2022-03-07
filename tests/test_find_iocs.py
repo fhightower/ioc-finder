25de80b539061fb2637eca8a28427f85dcc13178
nor3th <73965465+nor3th@users.noreply.github.com>
2022-03-07 14:40:35 +0100

    [tests] Implement change requests

--- tests/test_find_iocs.py
+++ tests/test_find_iocs.py
@@ -5,19 +5,18 @@ from .find_iocs_cases import ALL_TESTS
 
 
 @pytest.mark.parametrize("text, results, args", ALL_TESTS)
-def test_find_iocs(text: str, results: Dict, args: Dict):
+def test_find_iocs(text: str, results: Dict, args: Dict) -> None:
     # Parse input
     iocs = find_iocs(text, **args)
 
     # Get all keys
     ioc_finder_keys = _get_nonempty_keys(iocs)
-    # results_keys = _get_nonempty_keys(results)
     results_keys = list(results.keys())
 
     # Test output with expected results
     assert sorted(ioc_finder_keys) == sorted(
         results_keys
-    ), f"Key values don't match '{ioc_finder_keys} vs '{results_keys}'"
+    ), f"Key values don't match '{ioc_finder_keys}' vs '{results_keys}'"
     for key in ioc_finder_keys:
         # Compare lists
         if type(iocs[key]) == list:
@@ -29,13 +28,13 @@ def test_find_iocs(text: str, results: Dict, args: Dict):
                 _compare_lists(iocs[key][subkey], results[key][subkey])
 
 
-def _compare_lists(ioc_list: List, result_list: List):
+def _compare_lists(ioc_list: List, result_list: List) -> None:
     assert sorted(ioc_list) == sorted(
         result_list
     ), f"Expected result '{result_list}' did not match parsed IOCs '{ioc_list}'"
 
 
-def _get_nonempty_keys(input: Dict):
+def _get_nonempty_keys(input: Dict) -> List:
     result = []
     for key, item in input.items():
         # If item is an empty list
