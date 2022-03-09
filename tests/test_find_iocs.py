a551cf7e48114ebce685a1226328a9ec0ddc2026
Floyd Hightower <floyd.hightower27@gmail.com>
2022-03-09 07:58:45 -0500

    Fix lint errors

--- tests/test_find_iocs.py
+++ tests/test_find_iocs.py
@@ -3,6 +3,7 @@ from typing import Dict, List
 import pytest
 
 from ioc_finder import find_iocs
+from ioc_finder.ioc_finder import IndicatorData, IndicatorDict, IndicatorList
 
 from .find_iocs_cases import ALL_TESTS
 
@@ -22,22 +23,26 @@ def test_find_iocs(text: str, results: Dict, args: Dict) -> None:
     ), f"Key values don't match '{ioc_finder_keys}' vs '{results_keys}'"
     for key in ioc_finder_keys:
         # Compare lists
-        if type(iocs[key]) == list:
-            _compare_lists(iocs[key], results[key])
+        if isinstance(iocs[key], list):
+            _compare_lists(iocs[key], results[key])  # type: ignore
         # Compare sub dictionaries like for attack patterns
-        elif type(iocs[key]) == dict:
-            for subkey in iocs[key].keys():
-                assert subkey in results[key], f"Expected key '{key}' not found in {results[key]}"
-                _compare_lists(iocs[key][subkey], results[key][subkey])
+        elif isinstance(iocs[key], dict):
+            _compare_dicts(iocs[key], results[key])  # type: ignore
 
 
-def _compare_lists(ioc_list: List, result_list: List) -> None:
+def _compare_lists(ioc_list: IndicatorList, result_list: IndicatorList) -> None:
     assert sorted(ioc_list) == sorted(
         result_list
     ), f"Expected result '{result_list}' did not match parsed IOCs '{ioc_list}'"
 
 
-def _get_nonempty_keys(input: Dict) -> List:
+def _compare_dicts(ioc_dict: IndicatorDict, result_dict: IndicatorDict) -> None:
+    for key in ioc_dict.keys():
+        assert key in result_dict, f"Expected key '{key}' not found in {result_dict.keys()}"
+        _compare_lists(ioc_dict[key], result_dict[key])
+
+
+def _get_nonempty_keys(input: IndicatorData) -> List[str]:
     result = []
     for key, item in input.items():
         # If item is an empty list
