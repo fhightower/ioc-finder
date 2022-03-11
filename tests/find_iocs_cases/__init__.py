ed319a6b255bcdae4cd4a1ecfaa01090af044b6c
nor3th <73965465+nor3th@users.noreply.github.com>
2022-03-11 22:05:39 +0100

    [tests] Add registry tests

--- tests/find_iocs_cases/__init__.py
+++ tests/find_iocs_cases/__init__.py
@@ -6,7 +6,8 @@ from .hashes import HASH_DATA
 from .ip_addr import IP_DATA
 from .tlp_labels import TLP_DATA
 from .attack_data import ATTACK_DATA
+from .registry_keys import REGISTRY_DATA
 
-cases = [TLP_DATA, DOMAIN_DATA, EMAIL_DATA, HASH_DATA, CVE_DATA, IP_DATA, ASN_DATA, ATTACK_DATA]
+cases = [TLP_DATA, DOMAIN_DATA, EMAIL_DATA, HASH_DATA, CVE_DATA, IP_DATA, ASN_DATA, ATTACK_DATA, REGISTRY_DATA]
 
 ALL_TESTS = [val for sublist in cases for val in sublist]
