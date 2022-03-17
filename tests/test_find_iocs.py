f660140ac862e427ec28867dba9fb36f7ba50845
nor3th <73965465+nor3th@users.noreply.github.com>
2022-03-17 08:48:31 +0100

    [tests] Improve comparison handling. Empty lists are compared to avoid skipping over entries in the result_list

--- tests/test_find_iocs.py
+++ tests/test_find_iocs.py
@@ -15,8 +15,8 @@ def test_find_iocs(text: str, results: Dict, args: Dict) -> None:
 
     for key, value in iocs.items():
         # Compare lists
-        if isinstance(value, list) and len(value) > 0:
-            _compare_lists(key, value, results.get(key, []))  # type: ignore
+        if isinstance(value, list):
+            _compare_lists(key, value, results.get(key, []))
         # Compare sub dictionaries like for attack patterns
         elif isinstance(value, dict):
             _compare_dicts(key, value, results.get(key, {}))
@@ -30,5 +30,5 @@ def _compare_lists(key_name: str, ioc_list: IndicatorList, result_list: Indicato
 
 def _compare_dicts(key_name: str, ioc_dict: IndicatorDict, result_dict: IndicatorDict) -> None:
     for ioc_key, ioc_value in ioc_dict.items():
-        if isinstance(ioc_value, list) and len(ioc_value) > 0:
-            _compare_lists(f"{key_name},{ioc_key}", ioc_value, result_dict.get(ioc_key, {}))  # type: ignore
+        if isinstance(ioc_value, list):
+            _compare_lists(f"{key_name},{ioc_key}", ioc_value, result_dict.get(ioc_key, []))
