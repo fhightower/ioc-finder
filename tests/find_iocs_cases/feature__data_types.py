7b7733ed168c656bef14fdfc80de7fa0c53942e4
Floyd Hightower <floyd.hightower27@gmail.com>
2022-06-04 07:28:05 -0400

    Updating tests with expected results

--- tests/find_iocs_cases/feature__data_types.py
+++ tests/find_iocs_cases/feature__data_types.py
@@ -20,7 +20,7 @@ IOC_EXAMPLES = {
     'sha256s': ['a' * 64],
     'sha1s': ['a' * 40],
     'md5s': ['a' * 32],
-    'ssdeeps': ['12288:QYV6MorX7qzuC3QHO9FQVHPF51jgcSj2EtPo/V7I6R+Lqaw8i6hG0:vBXu9HGaVHh4Po/VU6RkqaQ6F'],
+    'ssdeeps': ['12288:QYV6MorX7qzuC3QHO9FQVHPF51jgcSj2EtPo/V7I6R+Lqaw8i6hG0:vBXu9HGaVHh4Po/VU6RkqaQ6F', '0000:0000:ff00', '2001:0db8:0000'],  # I don't like that the components of an ipv6 can be parsed as an ssdeep... I've ticketed this here: https://github.com/fhightower/ioc-finder/issues/228
     'asns': ['ASN123'],
     'cves': ['CVE-2022-1234'],
     'registry_key_paths': ['HKEY_LOCAL_MACHINE\Software\Microsoft\Windows'],
@@ -29,13 +29,13 @@ IOC_EXAMPLES = {
     'bitcoin_addresses': ['18ddf28a71089acdbab5038f58044c0a', '3J98t1WpEZ73CNmQviecrnyiWrnqRhWNLy'],
     'monero_addresses': ['496aKKdqF1xQSSEzw7wNrkZkDUsCD5cSmNCfVhVgEps52WERBcLDGzdF5UugmFoHMm9xRJdewvK2TFfAJNwEV25rTcVF5Vp'],
     'mac_addresses': ['AA-F2-C9-A6-B3-4F'],
-    'user_agents': ['Mozilla/4.0 (compatible; MSIE 7.0b; Windows NT 5.1; .NET CLR 1.1.4322; InfoPath.1)'],
+    'user_agents': ['Mozilla/4.0 (compatible; MSIE 7.0b; Windows NT 5.1; .NET CLR 1.1.4322; InfoPath.1) TLP'],  # I don't like this parsing... I've ticketed this for improvement here: https://github.com/fhightower/ioc-finder/issues/227
     'tlp_labels': ['TLP:RED'],
     'mac_addresses': ['AA-F2-C9-A6-B3-4F'],
     'file_paths': ['~/foo/bar/abc.py'],
     'attack_mitigations': {'enterprise': ['M1036', 'M1015']},
-    'attack_tactics': [],
-    'attack_techniques': [],
+    'attack_tactics': {'pre_attack': ['TA0012']},
+    'attack_techniques': {'pre_attack': ['T1329']},
 }
 all_ioc_text = ' '.join([val for sublist in IOC_EXAMPLES.values() for val in sublist])
 
@@ -43,6 +43,12 @@ all_ioc_text = ' '.join([val for sublist in IOC_EXAMPLES.values() for val in sub
 # imphashes and authentihashes require the hash to be prefixed with `imphash` and `authentihash` respectively, but when parsed, only the hash itself will be present
 all_ioc_text = all_ioc_text.replace(IOC_EXAMPLES['imphashes'][0], f'imphash {IOC_EXAMPLES["imphashes"][0]}')
 all_ioc_text = all_ioc_text.replace(IOC_EXAMPLES['authentihashes'][0], f'authentihash {IOC_EXAMPLES["authentihashes"][0]}')
+all_ioc_text = all_ioc_text.replace(IOC_EXAMPLES['user_agents'][0], IOC_EXAMPLES['user_agents'][0].rstrip(' TLP'))
+# add the attack data
+all_ioc_text = all_ioc_text + ' ' + ' '.join(IOC_EXAMPLES['attack_mitigations']['enterprise'])
+all_ioc_text = all_ioc_text + ' ' + ' '.join(IOC_EXAMPLES['attack_tactics']['pre_attack'])
+all_ioc_text = all_ioc_text + ' ' + ' '.join(IOC_EXAMPLES['attack_techniques']['pre_attack'])
+
 
 individual_data_types_tests = []
 
