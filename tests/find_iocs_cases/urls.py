bfb81ea1766e296993930e94aacf2735d17a39bd
Floyd Hightower <floyd.hightower27@gmail.com>
2022-06-13 05:08:45 -0400

    Remove unneeded tests

--- tests/find_iocs_cases/urls.py
+++ tests/find_iocs_cases/urls.py
@@ -2,12 +2,6 @@ from pytest import param
 
 URL_DATA = [
     param(
-        "1.1.1.1/0",
-        {"urls": []},
-        {"included_ioc_types": ["urls"]},
-        id="cidr_range_with_included_ioc_types_urls",
-    ),
-    param(
         "https://example.com/test%20page/foo.com/bingo.php?q=bar.com",
         {
             "urls": ["https://example.com/test%20page/foo.com/bingo.php?q=bar.com"],
@@ -15,11 +9,5 @@ URL_DATA = [
         },
         {},
         id="URL and domains parsed",
-    ),
-    param(
-        "https://example.com/test%20page/foo.com/bingo.php?q=bar.com",
-        {"urls": ["https://example.com/test%20page/foo.com/bingo.php?q=bar.com"]},
-        {"included_ioc_types": ["urls"]},
-        id="Only URL parsed when included_ioc_types is used",
     ),
 ]
