151c2e305bd7268bffe8813c9b8958ddfd832042
Floyd Hightower <floyd.hightower27@gmail.com>
2022-06-20 05:32:30 -0400

    Update quotes in top level files

--- setup.py
+++ setup.py
@@ -1,46 +1,46 @@
 from setuptools import setup, find_packages
 
-with open('README.md') as readme_file:
+with open("README.md") as readme_file:
     readme = readme_file.read()
 
-with open('requirements.txt') as requirements_file:
+with open("requirements.txt") as requirements_file:
     requirements = requirements_file.read().splitlines()
 
 setup(
-    name='ioc_finder',
-    version='7.1.0',
+    name="ioc_finder",
+    version="7.1.0",
     description="Python package for finding and parsing indicators of compromise from text.",
-    entry_points={'console_scripts': ['ioc-finder=ioc_finder.ioc_finder:cli_find_iocs']},
+    entry_points={"console_scripts": ["ioc-finder=ioc_finder.ioc_finder:cli_find_iocs"]},
     long_description=readme,
     long_description_content_type="text/markdown",
     author="Floyd Hightower",
-    author_email='',
-    url='https://github.com/fhightower/ioc-finder',
+    author_email="",
+    url="https://github.com/fhightower/ioc-finder",
     project_urls={
-        'Documentation': 'https://github.com/fhightower/ioc-finder#ioc-finder',
-        'Say Thanks!': 'https://saythanks.io/to/floyd.hightower27%40gmail.com',
-        'Source': 'https://github.com/fhightower/ioc-finder',
-        'Tracker': 'https://github.com/fhightower/ioc-finder/issues',
-        'PyPi': 'https://pypi.org/project/ioc-finder/',
-        'CI': 'https://github.com/fhightower/ioc-finder/actions',
-        'Changelog': 'https://github.com/fhightower/ioc-finder/blob/main/CHANGELOG.md',
+        "Documentation": "https://github.com/fhightower/ioc-finder#ioc-finder",
+        "Say Thanks!": "https://saythanks.io/to/floyd.hightower27%40gmail.com",
+        "Source": "https://github.com/fhightower/ioc-finder",
+        "Tracker": "https://github.com/fhightower/ioc-finder/issues",
+        "PyPi": "https://pypi.org/project/ioc-finder/",
+        "CI": "https://github.com/fhightower/ioc-finder/actions",
+        "Changelog": "https://github.com/fhightower/ioc-finder/blob/main/CHANGELOG.md",
     },
-    packages=find_packages(exclude=('tests', 'docs')),
+    packages=find_packages(exclude=("tests", "docs")),
     include_package_data=True,
     install_requires=requirements,
-    license='GNU Lesser General Public License v3',
+    license="GNU Lesser General Public License v3",
     zip_safe=True,
-    keywords='iocs,indicators of compromise,parsing,finding,searching,threat intelligence,malware,threat hunting,observables,domains,domain names,asns,cidr,cidr ranges,ips,ip addresses,urls,email addresses,md5,sha1,sha256,google ads,cve,file paths',
+    keywords="iocs,indicators of compromise,parsing,finding,searching,threat intelligence,malware,threat hunting,observables,domains,domain names,asns,cidr,cidr ranges,ips,ip addresses,urls,email addresses,md5,sha1,sha256,google ads,cve,file paths",
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
-    test_suite='tests',
+    test_suite="tests",
 )
--- utility.py
+++ utility.py
@@ -3,47 +3,47 @@
 
 import requests
 
-PRE_ATTACK_URL = 'https://raw.githubusercontent.com/mitre/cti/master/pre-attack/pre-attack.json'
-ENTERPRISE_ATTACK_URL = 'https://raw.githubusercontent.com/mitre/cti/master/enterprise-attack/enterprise-attack.json'
-MOBILE_ATTACK_URL = 'https://raw.githubusercontent.com/mitre/cti/master/mobile-attack/mobile-attack.json'
+PRE_ATTACK_URL = "https://raw.githubusercontent.com/mitre/cti/master/pre-attack/pre-attack.json"
+ENTERPRISE_ATTACK_URL = "https://raw.githubusercontent.com/mitre/cti/master/enterprise-attack/enterprise-attack.json"
+MOBILE_ATTACK_URL = "https://raw.githubusercontent.com/mitre/cti/master/mobile-attack/mobile-attack.json"
 
 
 def _get_id(data):
-    return data['external_references'][0]['external_id']
+    return data["external_references"][0]["external_id"]
 
 
 def get_pre_attack_data():
     r = requests.get(PRE_ATTACK_URL)
-    l = r.json()['objects']
-    tactics = [_get_id(i) for i in l if i['type'] == 'x-mitre-tactic']
-    techniques = [_get_id(i) for i in l if i['type'] == 'attack-pattern']
+    l = r.json()["objects"]
+    tactics = [_get_id(i) for i in l if i["type"] == "x-mitre-tactic"]
+    techniques = [_get_id(i) for i in l if i["type"] == "attack-pattern"]
     return tuple(tactics), tuple(techniques)
 
 
 def get_enterprise_attack_data():
     r = requests.get(ENTERPRISE_ATTACK_URL)
-    d = r.json()['objects']
-    tactics = [_get_id(i) for i in d if i['type'] == 'x-mitre-tactic']
-    techniques = [_get_id(i) for i in d if i['type'] == 'attack-pattern']
-    mitigations = [_get_id(i) for i in d if i['type'] == 'course-of-action' and _get_id(i).startswith('M')]
+    d = r.json()["objects"]
+    tactics = [_get_id(i) for i in d if i["type"] == "x-mitre-tactic"]
+    techniques = [_get_id(i) for i in d if i["type"] == "attack-pattern"]
+    mitigations = [_get_id(i) for i in d if i["type"] == "course-of-action" and _get_id(i).startswith("M")]
     return tuple(tactics), tuple(techniques), tuple(mitigations)
 
 
 def get_mobile_attack_data():
     r = requests.get(MOBILE_ATTACK_URL)
-    d = r.json()['objects']
-    tactics = [_get_id(i) for i in d if i['type'] == 'x-mitre-tactic']
-    techniques = [_get_id(i) for i in d if i['type'] == 'attack-pattern']
-    mitigations = [_get_id(i) for i in d if i['type'] == 'course-of-action' and _get_id(i).startswith('M')]
+    d = r.json()["objects"]
+    tactics = [_get_id(i) for i in d if i["type"] == "x-mitre-tactic"]
+    techniques = [_get_id(i) for i in d if i["type"] == "attack-pattern"]
+    mitigations = [_get_id(i) for i in d if i["type"] == "course-of-action" and _get_id(i).startswith("M")]
     return tuple(tactics), tuple(techniques), tuple(mitigations)
 
 
 def get_tlds():
     """."""
-    r = requests.get('https://data.iana.org/TLD/tlds-alpha-by-domain.txt')
-    tlds = r.text.split('\n')[1:-1]
+    r = requests.get("https://data.iana.org/TLD/tlds-alpha-by-domain.txt")
+    tlds = r.text.split("\n")[1:-1]
     tlds = [i.lower() for i in tlds]
-    tlds.append('onion')
+    tlds.append("onion")
     return tuple(tlds)
 
 
