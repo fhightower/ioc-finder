6d4f6d2a0515dcdaf5390ab4319754d203b5e5f7
Floyd Hightower <floyd.hightower27@gmail.com>
2022-09-27 06:22:14 -0400

    Fix script to fail when appropriate

--- docker/lint.sh
+++ docker/lint.sh
@@ -11,7 +11,7 @@ black ioc_finder/ tests/
 # if the CONTEXT env var is "ci" (which is set in .github/workflows/lint.yml), validate that none of the files
 # have been changed by the previous lint steps
 if [ "${CONTEXT:-local}" = "ci" ]; then
-    git status | grep "nothing to commit" || echo "Lint steps have changed files" && exit 1;
+    (git status | grep "nothing to commit") || { echo "Lint steps have changed files"; exit 1; };
 fi
 
 mypy ioc_finder/ tests/
